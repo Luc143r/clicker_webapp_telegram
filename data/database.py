2e9ad8828cd71c927a27f9a7caa24ef322f6d1ae
luc143r <nikves29012001@gmail.com>
2024-01-31 21:55:38 +0300

    push database module

--- data/database.py
+++ data/database.py
@@ -4,11 +4,10 @@ import pymysql
 def db_session(method):
     def wrapper(self, *args, **kwargs):
         connection = pymysql.connect(
-            host='127.0.0.1',
-            port=8000,
+            host='localhost',
+            port=3306,
             user='root',
-            password='admin',
-            database='points',
+            password='root',
             cursorclass=pymysql.cursors.DictCursor
         )
         cursor = connection.cursor()
@@ -23,10 +22,23 @@ def db_session(method):
 class Database:
     @db_session
     def __init__(self, cursor: pymysql.cursors.DictCursor):
-        cursor.execute("CREATE TABLE IF NOT EXISTS `point`(id int AUTO_INCREMENT," \
-                        "user_id int," \
-                        "username varcher(32)," \
+        cursor.execute("CREATE DATABASE IF NOT EXISTS `boobscoin`")
+        cursor.execute("USE `boobscoin`")
+        cursor.execute("CREATE TABLE IF NOT EXISTS `users`(id int AUTO_INCREMENT," \
+                        "user_id varchar(10)," \
+                        "username varchar(32)," \
                         "point int," \
                         "PRIMARY KEY(id))")
         
+    @db_session
+    def add_user(self, user_id: str, username: str, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("INSERT INTO users (user_id, username, point) VALUES (%s, %s, %s)", (user_id, username, 0))
         
+    @db_session
+    def get_user(self, user_id: str, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id))
+        return cursor.fetchone()
+        
+    @db_session
+    def update_user_point(self, user_id: str, point: int, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("UPDATE users SET point = %s WHERE user_id = %s", (point, user_id))
