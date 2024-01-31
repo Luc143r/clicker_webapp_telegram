71b8ca01d572cd3fa8e35d4d094a7be90efe6b4d
luc143r <nikves29012001@gmail.com>
2024-01-31 23:27:39 +0300

    change database.py

--- data/database.py
+++ data/database.py
@@ -32,18 +32,22 @@ class Database:
         
     @db_session
     def add_user(self, user_id: str, username: str, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("USE `boobscoin`")
         cursor.execute("INSERT INTO users (user_id, username, point) VALUES (%s, %s, %s)", (user_id, username, 0))
         
     @db_session
     def get_user(self, user_id: str, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("USE `boobscoin`")
         cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id))
         return cursor.fetchone()
     
     @db_session
     def get_all_users(self, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("USE `boobscoin`")
         cursor.execute("SELECT * FROM users ORDER BY point DESC")
         return cursor.fetchall()
         
     @db_session
     def update_user_point(self, user_id: str, point: int, cursor: pymysql.cursors.DictCursor):
+        cursor.execute("USE `boobscoin`")
         cursor.execute("UPDATE users SET point = %s WHERE user_id = %s", (point, user_id))
