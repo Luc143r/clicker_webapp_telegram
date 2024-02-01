81e14396b10bf121a8c1c57772870f298a49b46b
luc143r <nikves29012001@gmail.com>
2024-02-01 22:38:48 +0300

    Переделал запросы через транзакции. Пофиксил дублирование записей в базе

--- data/database.py
+++ data/database.py
@@ -1,7 +1,7 @@
 import pymysql
 
 
-def db_session(method):
+'''def db_session(method):
     def wrapper(self, *args, **kwargs):
         connection = pymysql.connect(
             host='localhost',
@@ -12,10 +12,48 @@ def db_session(method):
         )
         cursor = connection.cursor()
         try:
+            connection.begin()
             return method(self, *args, **kwargs, cursor=cursor)
+        except pymysql.err.IntegrityError as err:
+            if err.args[0] == 1062:
+                print('Дублирование данных. Пользователь уже зарегистрирован')
+                connection.rollback()
         finally:
             connection.commit()
+            cursor.close()
             connection.close()
+    return wrapper'''
+
+
+def db_session(method):
+    def wrapper(self, *args, **kwargs):
+        connection = pymysql.connect(
+            host='localhost',
+            port=3306,
+            user='root',
+            password='root',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        try:
+            cursor = connection.cursor()
+            connection.begin()
+            try:
+                result = method(self, *args, **kwargs, cursor=cursor)
+                connection.commit()
+                return result
+            except Exception as e:
+                print(f'Ошибка: {e}')
+                connection.rollback()
+                print('Откат изменений')
+                cursor.execute("ALTER TABLE `boobscoin`.`users` AUTO_INCREMENT = 1")
+                connection.commit()
+            finally:
+                cursor.close()
+        except Exception as err:
+            print(f'Произошла ошибка подключения: {err}')
+        finally:
+            if connection:
+                connection.close()
     return wrapper
 
 
@@ -25,7 +63,7 @@ class Database:
         cursor.execute("CREATE DATABASE IF NOT EXISTS `boobscoin`")
         cursor.execute("USE `boobscoin`")
         cursor.execute("CREATE TABLE IF NOT EXISTS `users`(id int AUTO_INCREMENT," \
-                        "user_id varchar(10)," \
+                        "user_id varchar(10) UNIQUE KEY," \
                         "username varchar(32)," \
                         "point int," \
                         "PRIMARY KEY(id))")
